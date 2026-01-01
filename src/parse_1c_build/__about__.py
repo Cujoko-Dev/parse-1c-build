f9137bc6d1d786d01348caf6a1e871a5a8539223
Cujoko <cujoko@gmail.com>
2026-01-01 21:19:23 +0900

    Обновит __about__.py

--- src/parse_1c_build/__about__.py
+++ src/parse_1c_build/__about__.py
@@ -3,4 +3,4 @@ import importlib.metadata
 APP_AUTHOR = "util-1c"
 APP_NAME = "parse-1c-build"
 
-__version__ = importlib.metadata.version(__package__)
+__version__ = importlib.metadata.version(str(__package__))
