6e7259fdeeea579784023cfd81fcf49cb5cd0600
Cujoko <cujoko@gmail.com>
2026-01-01 21:20:18 +0900

    Обновит __main__.py

--- src/parse_1c_build/__main__.py
+++ src/parse_1c_build/__main__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from parse_1c_build.core import run
 
-sys.path.insert(0, Path(__file__).parent.parent)
+sys.path.insert(0, str(Path(__file__).parent.parent))
 
 if __name__ == "__main__":
     run()
