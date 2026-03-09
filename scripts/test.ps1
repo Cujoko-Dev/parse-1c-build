2a0089179d31cf8fbbbe0d81bf1efe30dc5833da
Cujoko <cujoko@gmail.com>
2026-03-10 00:31:23 +0900

    Улучшит test.ps1

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,1 +1,10 @@
-pytest
+$ErrorActionPreference = "Stop"
+
+if (!(Test-Path ".venv")) {
+    Write-Host "Virtual environment not found"
+    exit 1
+}
+
+.venv\Scripts\activate.ps1
+
+pytest -q --maxfail=1
