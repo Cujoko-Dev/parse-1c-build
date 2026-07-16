41a300a7c3bc480771e33cb45649a4e47ef87364
Cujoko <cujoko@gmail.com>
2026-07-16 23:09:55 +0900

    refactor: centralize package version metadata

--- src/parse_1c_build/__about__.py
+++ src/parse_1c_build/__about__.py
@@ -1,6 +1,4 @@
-import importlib.metadata
+from parse_1c_build._version import __version__
 
 APP_AUTHOR = "util-1c"
 APP_NAME = "parse-1c-build"
-
-__version__ = importlib.metadata.version(str(__package__))
